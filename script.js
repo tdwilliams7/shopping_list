b9d447831dda7f5664fcbbd9116b3510904aded4
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-10-17 13:37:39 -0500

    add checkbox. add error message for 0 length input. add delete onclick.

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
 let list = document.getElementsByTagName('ul')[0];
 
 const newFood = document.getElementById('newFoodInput');
-let foodName = newFood;
+
 
 // let addNewItem = document.createElement('li');
 // let newItemText = document.createTextNode('onions');
 // addNewItem.appendChild(newItemText);
-// list.appendChild(addNewItem);
+// // list.appendChild(addNewItem);
+// eventHandlers ={
+//   addButton: function(){
+//     let addButtonEl = document.getElementById('addFoodButton');
+//     addButtonEL.addEventListener('click', function(){handlers.addNewItem();});
+//   }
+// }
+
+// handlers = {
+//   addNewItem: function() {
+//   let addNewItem = document.createElement('li');
+//   let newItemText = document.createTextNode(newFood.value);
+//   addNewItem.appendChild(newItemText);
+//   list.appendChild(addNewItem);
+//   let deleteButton = document.createElement('button');
+//   let buttonText = document.createTextNode('Delete');
+//   deleteButton.appendChild(buttonText);
+//   deleteButton.setAttribute('id', 'l' + (document.querySelectorAll('li').length))
+//   list.appendChild(deleteButton);
+//   newFood.value.innerHTML = '';
+//   },
+
+// }
+
+let alert = document.getElementById('alert');
 
 
-handlers = {
-  addNewItem: function() {
+var addFoodItemButton = document.getElementById('addFoodButton');
+addFoodItemButton.addEventListener('click', function(){
+  if (newFood.value.length > 0){
   let addNewItem = document.createElement('li');
   let newItemText = document.createTextNode(newFood.value);
   addNewItem.appendChild(newItemText);
-  list.appendChild(addNewItem);
+  list.insertBefore(addNewItem, list.firstChild);
+  let check = document.createElement('input');
+  check.setAttribute('type', 'checkbox');
+  addNewItem.appendChild(check);
   let deleteButton = document.createElement('button');
   let buttonText = document.createTextNode('Delete');
   deleteButton.appendChild(buttonText);
   deleteButton.setAttribute('id', 'l' + (document.querySelectorAll('li').length))
-  list.appendChild(deleteButton);
-  },
+  deleteButton.setAttribute('onClick', 'deleteFoodButton(this)');
+  addNewItem.appendChild(deleteButton);
+
+  newFood.value = '';
+
+  alert.textContent = '';
+  } else {
+
+    alert.textContent = 'Please enter a food Name';
+  }
+
+});
+
+
+function deleteFoodButton(e){
+
+  e.parentNode.remove(e.id)
 
 }
